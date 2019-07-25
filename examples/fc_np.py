1b8b2d8617e0facd43ec31eb445fd4e7016e151b
Cristiano-3 <chunanluo@126.com>
2019-07-25 17:21:17 +0800

    update comments in fc_np.py

--- examples/fc_np.py
+++ examples/fc_np.py
@@ -14,8 +14,10 @@ y = np.random.randn(N, D_out)
 w1 = np.random.randn(D_in, H)
 w2 = np.random.randn(H, D_out)
 
+# epoch=500, batch=whole_dataset
 learning_rate = 1e-6
 for t in range(500):
+    # part 1 -----------------------------
     # forward
     h = x.dot(w1)
     h_relu = np.maximum(h, 0)
@@ -25,6 +27,7 @@ for t in range(500):
     loss = np.square(y_pred - y).sum()
     print(t, loss)
 
+    # part 2 -----------------------------
     # backward, 
     # 连式法则求导 grad_w1 和 grad_w2, 
     # 求 grad_w2 时注意 relu 的地方
